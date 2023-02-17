d0b8364d188bd5551ea74d8eafdbffd32a78d7cc
Hiroki Maeda <maetyanidattyo169@gmail.com>
2023-02-17 20:20:49 +0900

    test

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ app.secret_key = uuid.uuid4().hex
 app.permanent_session_lifetime = timedelta(days=30)
 
 #app.config['MAX_CONTENT_LENGTH'] = 70 * 1024 * 1024
+#test
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
